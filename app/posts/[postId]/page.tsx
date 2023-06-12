242aff33e51bf56c97d6f63f7d3b3b70da9e3ef1
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-11 22:46:19 -0300

    feat: post page

---
+++ app/posts/[postId]/page.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import IPost from "@/interfaces/IPost";
+import IUser from "@/interfaces/IUser";
+import getPostById from "@/lib/getPostById";
+import getUserById from "@/lib/getUserById";
+
+interface IParams {
+  params: { postId: string };
+}
+
+export default async function Post({ params }: IParams) {
+  const { postId } = params;
+  const { title, body, userId }: IPost = await getPostById(postId);
+  const user: IUser | undefined = await getUserById(userId);
+
+  const paragraphs = body.split("\n");
+  const formattedBody = paragraphs.map((paragraph, index) => (
+    <p key={index} className="first-letter:capitalize">
+      {paragraph}
+    </p>
+  ));
+
+  return (
+    <section className="max-w-lg mx-auto">
+      <h2 className="mb-2 text-3xl font-bold capitalize">{title}</h2>
+      <div className="my-2 border-b-2 border-slate-300"></div>
+      <h3 className="mb-6 text-sm">Autor: {user?.name}</h3>
+      <div className="flex flex-col gap-2 text-2xl">{formattedBody}</div>
+
+      <div className="mt-6 text-sm hover:underline">
+        <Link href="/">{`< Return Home`}</Link>
+      </div>
+    </section>
+  );
+}
