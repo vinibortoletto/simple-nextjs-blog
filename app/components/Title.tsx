7a37cdf974e8321f004d43e3d49d378718666943
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-11 23:26:45 -0300

    feat: add link to logo

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -1,11 +1,12 @@
 import { Bebas_Neue } from "next/font/google";
+import Link from "next/link";
 
 const bebasNeue = Bebas_Neue({ subsets: ["latin"], weight: "400" });
 
 export default function Title() {
   return (
     <h1 className={`${bebasNeue.className} text-6xl text-center my-6`}>
-      Simple Blog
+      <Link href="/">Simple Blog</Link>
     </h1>
   );
 }
